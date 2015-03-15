df83d71e04db4dc62049baa1fc5290a25ca6ae21
richard lum <a00470425@gmail.com>
2015-03-14 23:17:01 -0700

    reset compiles

--- shptr.cpp
+++ shptr.cpp
@@ -19,6 +19,10 @@ public:
 	int getY(){
 		return ypos;
 	}
+	void setPos(int x, int y) {
+		xpos = x;
+		ypos = y;
+	}
 	std::string whatami() {
 		return "Shape at (" +  tostr(xpos) + "," + tostr(ypos) + ").";	
 	}
@@ -28,10 +32,11 @@ private:
 	}
 };
 
+typedef  std::vector<std::shared_ptr<Shape*>> shapevector;
 
-
-std::vector<std::shared_ptr<Shape*>>* populate(int qty){
-	std::vector<std::shared_ptr<Shape*>>* shapes = new std::vector<std::shared_ptr<Shape*>>(); // = new std::vector<Shape*>();
+//std::vector<std::shared_ptr<Shape*>>* populate(int qty){
+shapevector* populate(int qty){
+	shapevector* shapes = new std::vector<std::shared_ptr<Shape*>>(); // = new std::vector<Shape*>();
 	//std::shared_ptr<Shape> s1 = std::make_shared<Shape>( Shape (1,1) );
 	for (int i = 0;i<qty;i++){
 		shapes->push_back( std::make_shared<Shape*>( new Shape (i,i+1) ));
@@ -39,10 +44,24 @@ std::vector<std::shared_ptr<Shape*>>* populate(int qty){
 	return shapes;
 }
 
+shapevector* move(int x , int y, shapevector* sv) {
+	for (std::shared_ptr<Shape*> s : *sv ) {
+		(*s)->setPos( (*s)->getX() + x, (*s)->getY() + y);
+	}
+	return sv;
 
+}
+
+void empty (shapevector* sv){
+	for (std::shared_ptr<Shape*> s : *sv) {
+		s.reset();  // note reset is a shared_ptr method  
+	}
+		
+}
 int main(int argc, char** argv) {
 
-	std::vector<std::shared_ptr<Shape*>>* shapes = populate(5); 
+	shapevector* shapes = populate(5);
+	shapes = move(3,2,shapes); 
 	int i = 0;
 	for (auto s : *shapes) {
 		std::cout << ++i << " : " << (*s)->whatami() << std::endl;
